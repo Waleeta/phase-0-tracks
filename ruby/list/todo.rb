4ece9e1ffbdbdc4807d1fa0ab7819468d105dd97
Waleeta Canon <waleetacanon@gmail.com>
2016-08-20 14:23:29 -0500

    Adds To-do list rspec

---
+++ ruby/list/todo.rb
@@ -0,0 +1,19 @@
+class Todolist
+  
+  def list
+    list = []
+  end
+
+  def add_item(item)
+    list << item
+  end
+
+  def delete_item(item)
+    list.delete(item)
+  end
+
+  def get_item(index)
+    list[index]
+  end
+
+end
