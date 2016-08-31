c415d1f3e485357a3adb668f2afd21c9078744ac
Waleeta Canon <waleetacanon@gmail.com>
2016-08-31 12:12:48 -0500

    Adds comments to each method.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,31 +4,31 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+"iNvEsTiGaTiOn".swapcase #swaps each char for opposite case.
 
 # => “InVeStIgAtIoN”
 
 "zom"
-"zom".insert(1,"o")
+"zom".insert(1,"o") #inserts at index number, char
 "zom".insert(2,"o")
-"zom".sub("o", "oo")
+"zom".sub("o", "oo") #substitutes patter for another pattern.
 # => “zoom”
 
 "enhance"
-"enhance".center(15)
+"enhance".center(15) #centers with '15' (or another integer) number of spaces on each side. Can also add other characters instead of spaces.
 # => "    enhance    "
 
-"Stop! You’re under arrest!".upcase
+"Stop! You’re under arrest!".upcase #makes every letter capital.
 # => "STOP! YOU’RE UNDER ARREST!"
 
-"the usual" + " suspects"
+"the usual" + " suspects" #two strings can be added.
 OR 
-a = "suspects"
-a.prepend("the usual ")
+a = "suspects" #set variable to a str
+a.prepend("the usual ") #'prepend' inserts str BEFORE original string.
 #=> "the usual suspects"
 
 OR
-"suspects".prepend("the usual ")
+"suspects".prepend("the usual ") #can use 'prepend' without setting it to a variable first 
 # " suspects".<???>
 # => "the usual suspects"
 
@@ -37,21 +37,21 @@ OR
 # => "The case of the disappearing last lette"
 
 "The mystery of the missing first letter"
-"The mystery of the missing first letter".delete("T")
+"The mystery of the missing first letter".delete("T") #deletes char, but not at index. Any matching char.
 OR
-"The mystery of the missing first letter".slice(1..-1)) #use "p" or "puts" to print.
+"The mystery of the missing first letter".slice(1..-1)) #slices but returns sliced chars - index clarifies which chars to return.
 # => "he mystery of the missing first letter"
 
 "Elementary,    my   dear        Watson!"
-"Elementary,    my   dear        Watson!".squeeze
+"Elementary,    my   dear        Watson!".squeeze #removes all byt one space.
 # => "Elementary, my dear Watson!"
 
 "z"
-"z".ord
+"z".ord #returns ASCII value.
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 	#THAT IS ITS PLACE ON THE ASCII INDEX.
 
 "How many times does the letter 'a' appear in this string?"
-"How many times does the letter 'a' appear in this string?".count("a")
+"How many times does the letter 'a' appear in this string?".count("a") #counts given char in a str
 # => 4
