1781cfa7171afa07c888109144ef2f3ed3cabae2
Waleeta Canon <waleetacanon@gmail.com>
2016-09-01 15:49:27 -0500

    Adds iterative methods to hash and array.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -20,3 +20,43 @@ def greetings
 end
 
 greetings {|greeting| puts greeting}
+
+#ITERATE THROUGH ARRAYS AND HASHES:
+
+band_names = ["Killdozer", "Man or Astro Man", "Shellac", "Tune Yards", "Jawbreaker"]
+
+futurama_characters = {	pilot: "Turanga Leela",
+	professor: "Hubert Farnsworth",
+	doctor: "John Zoidberg",
+	bureaucrat: "Hermes Conrad",
+	intern: "Amy Wong",
+	pet: "Nibbler",
+	robot:"Bender Rodriguez"
+}
+
+#Bands returned UPCASE but ARRAY isn't modified
+band_names.each do |band|
+	band.upcase
+end 
+
+p band_names
+
+#ARRAY is changed to UPCASE
+band_names.map! do |band|
+	band.upcase
+end 
+
+p band_names
+
+
+futurama_characters.each do |title, name|
+	puts "#{name}'s title is #{title}."
+end 
+p futurama_characters
+
+#Can also be in one line but tried it with two: 
+futurama_characters.map do |title, name|
+	puts name
+	puts "is a #{title.upcase}."
+end 
+p futurama_characters
