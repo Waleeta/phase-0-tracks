95517a08fbf9c5b4191757ccd6ed8ae89179961f
Waleeta Canon <waleetacanon@gmail.com>
2016-09-03 11:43:45 -0500

    Adds Fibonacci method.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -4,13 +4,14 @@
 
 
 
-def search_array(array1, int)
-	if array1.include?(int)
-		p array1.index(int)
-else 
-	nil
-end 
+def search_array(item1, int)
+	if item1.include?(int)
+		p item1.index(int)
+	else
+		return false 
 	end 
+	
+end
 
 #DRIVER CODE:
 
@@ -19,3 +20,20 @@ search_array([3, 6, 19, 33, 4, 23], 19)
 #Define a new method that takes in an int 
 #Should return an array of terms of Fibonacci numbers
 #Method should do int.times add the next number to the previous number 
+
+
+def fib(num)
+  fib_array = []
+  num1 = 0 #assigning num1 to equal 0
+  num2 = 1 #assigning num2 to equal 1
+  
+  num.times do #iterating through each number 5 times 
+  	new_num = num1 
+  	fib_array << num1 = num2 
+  	
+  	num2 = new_num + num1
+  end
+  return fib_array
+end
+
+fib(6)
