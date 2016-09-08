8f46a6dbf676bc89e7d6c813017bb7e68384d845
Waleeta Canon <waleetacanon@gmail.com>
2016-09-08 13:36:34 -0500

    Initial Commit.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,40 +1,16 @@
-# module Shout
-#   def self.yell_angrily(words)
-#     words + "!!!" + " :("
-#   end
+module Shout 
 
-#   def self.yell_happily(words)
-#   	words + "!!!" + " :D"
-#   end 
-
-# end
-
-# p Shout.yell_angrily("WHAT")
-# p Shout.yell_happily("what")
-
-module Shout
-  def yell_angrily(words)
-    words + "!!!" + " :("
-  end
-
-  def yell_happily(words)
-  	words + "!!!" + " :D"
-  end 
-
-end
-
-class Angry_Woman
-	include Shout
+	def self.yell_angrily(words)
+    	words + "!!!" + " :("
+  	end
+	
+	def self.yell_happily(words)
+		words + "!!!!" + " ;-D"	
+	end
 end 
 
-class Angry_Man 
-	include Shout
-end
 
-woman = Angry_Woman.new 
-p woman.yell_angrily("WHAT")
-p woman.yell_happily("Why hello there")
 
-man = Angry_Man.new 
-p man.yell_angrily("WHAT ARE YOU LOOKG AT")
-p man.yell_happily("Yes of course")
+#DRIVER CODE:
+p Shout.yell_angrily("WHAT THE F___")
+p Shout.yell_happily("OH MY GOD")
