f1c5ac5b51e690bd378e7c326b4bc3ab54c18e5d
Waleeta Canon <waleetacanon@gmail.com>
2016-09-09 13:28:24 -0500

    Adds final print method and refactored code.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,49 +1,44 @@
-# Method to create a list
-# input: string of items separated by spaces (example: "carrots apples cereal pizza")
-# steps: 
-  # create an empty hash to store item and quantity
-  	#define a method that takes a string as an argument, the string will be split and then each 
-  		#item pushed into the hash with a default quantity.
-  		# set default quantity (set it to 1)
-  # print the list to the console [can you use one of your other methods here?] 
-  	#we can create a different that prints the list with the items: quantities.
-# output: [hash]
-
 def create_list(items)
-	items_array = items.split(" ")
 	grocery_hash = {}
-	items_array.each do |item|
-		grocery_hash[item] = 1
-	end 
-	grocery_hash	
+	items.split.each { |item| grocery_hash[item] = 1 }
+print_list(grocery_hash)
+return grocery_hash
 end 
 
-grocery_list = {}
-grocery_list.merge!(create_list("apples pears oranges"))
+#grocery_list = create_list("apples pears oranges")
 
-# Method to add an item to a list
-# input: item name and optional quantity
-# steps: push items and their quantities into the empty hash
-# output: hash
+#p grocery_list
+
 
 def adds_item(item, quantity, grocery_list)
 	grocery_list[item] = quantity
 end 
 
-adds_item("eggs", 1, grocery_list)
-p grocery_list
+#adds_item("eggs", 1, grocery_list)
+#p grocery_list
 
+def remove_item(item, grocery_list)
+	grocery_list.delete(item)
+end
+
+#remove_item("apples",grocery_list)
+#p grocery_list
+
+def update(item, grocery_list, quantity)
+	grocery_list[item] = quantity
+end
+
+#update("eggs", grocery_list, 2)
+#p grocery_list
+
+def print_list(grocery_list)
+	grocery_list.each do |item, quantity |
+		puts " #{item}: #{quantity} "
+	end
+end
+grocery_list = create_list("apples pears oranges")
+print_list(grocery_list)
 # Method to remove an item from the list
 # input: item
 # steps: delete the item by its name
 # output: updated hash
-
-# Method to update the quantity of an item
-# input: item
-# steps: update the item to be equal to a different quantity
-# output: updated hash
-
-# Method to print a list and make it look pretty
-# input: grocery list hash
-# steps: iterate through the hash, print its item: quantity
-# output: list
