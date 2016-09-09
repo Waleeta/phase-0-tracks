2065148be063f5a0ead97132be7a6379231e0065
Waleeta Canon <waleetacanon@gmail.com>
2016-09-09 11:15:10 -0500

    Initial commit.

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,30 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # create an empty hash to store item and quantity
+  	#define a method that takes a string as an argument, the string will be split and then each 
+  		#item pushed into the hash with a default quantity.
+  		# set default quantity (set it to 1)
+  # print the list to the console [can you use one of your other methods here?] 
+  	#we can create a different that prints the list with the items: quantities.
+# output: [hash]
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: push items and their quantities into the empty hash
+# output: hash
+
+# Method to remove an item from the list
+# input: item
+# steps: delete the item by its name
+# output: updated hash
+
+# Method to update the quantity of an item
+# input: item
+# steps: update the item to be equal to a different quantity
+# output: updated hash
+
+# Method to print a list and make it look pretty
+# input: grocery list hash
+# steps: iterate through the hash, print its item: quantity
+# output: list
