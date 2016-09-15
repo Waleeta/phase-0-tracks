5b378806c1a34cd4854fbf8ff43dca0464e95728
Waleeta Canon <waleetacanon@gmail.com>
2016-09-15 11:25:43 -0500

    Adds object-creator.

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,14 @@ var horse_names = ["Ed", "Secretariat", "Squirrel the Horse", "Black Beauty"];
 
 horse_colors.push("cream");
 horse_names.push("Magic");
+
+var horse_object = {};
+
+for (var index = 0; index <= horse_colors.length; index++) {
+	horse_object[horse_names[index]] = horse_colors[index];
+	horse_object;
+}
+
+
+//DRIVER CODE: 
+console.log(horse_object);
