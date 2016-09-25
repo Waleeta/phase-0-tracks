e9da99322c55be22d29d0991cc361ff0e1b42c0b
Waleeta Canon <waleetacanon@gmail.com>
2016-09-25 11:30:27 -0500

    Adds create table string.

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -9,10 +9,21 @@ require 'faker'
 # Create SQLite3 database
 # Setting it to a variable ("db") helps us interact with it.
 db = SQLite3::Database.new("kittens.db") #THIS MAKES THE DATABASE
+#db.execute("SELECT * FROM kittens")
 
 #learn about fancy string delimiters
+create_table_cmd = <<-SQL 
+	CREATE TABLE kittens(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		age INT
+		)
+SQL
+
 
 # create a kittens table (if it's not there already)
+
+db.execute(create_table_cmd)
 
 # add a test kitten
 
