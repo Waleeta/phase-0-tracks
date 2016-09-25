9a111bf9c3035fe36347b077247ba3f005c1fb3b
Waleeta Canon <waleetacanon@gmail.com>
2016-09-25 11:17:03 -0500

    Reverts kitten_maker code to empty.

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -6,44 +6,20 @@
 require 'sqlite3'
 require 'faker'
 
-
-
 # create SQLite3 database
-db = SQLite3::Database.new("kittens.db")
-#db.results_as_hash = true
-db.execute("SELECT * FROM kittens")
 
-# learn about fancy string delimiters
-create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS kittens(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    age INT
-  )
-SQL
+#learn about fancy string delimiters
 
-# # create a kittens table (if it's not there already)
-# db.execute(create_table_cmd)
+# create a kittens table (if it's not there already)
 
-# # add a test kitten
-# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+# add a test kitten
 
-# # add LOOOOTS of kittens!
-# # so. many. kittens. 
-# #KittenExplosion
-# def create_kitten(db, name, age)
-#   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
-# end
+# explore ORM by retrieving data
 
-# 100.times do
-#   create_kitten(db, Faker::Name.name, rand(0...20))
-# end
+# add LOOOOTS of kittens!
+# so. many. kittens. 
+#KittenExplosion
 
-# # explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
-# # puts kittens.class
-# # p kittens
-# kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
-# end
+
+
 
