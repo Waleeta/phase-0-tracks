cc8c92482e6004010f5284476ed7ec109d6fca89
Waleeta Canon <waleetacanon@gmail.com>
2016-09-29 12:36:16 -0500

    Adds routes to add route parameters, updates db

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -2,45 +2,82 @@
 require 'sinatra'
 require 'sqlite3'
 
+# get "/" do
+#   name = params[:name]
+#   age = params[:age]
+#   if name && age
+#     "<h1>Hello, #{name}! You are #{age} years old!</h1>"
+#   else
+#     "<h1>Hello, you!</h1>"
+#   end
+# end
+
+get "/" do
+  #http://localhost:9393/?name=waleeta&age=38
+  #query parameters
+    "<h1>Hello, #{params[:name]}! You are #{params[:age]} years old!</h1>"
+end
+
+get "/about/:person" do
+  #route parameter
+  #http://localhost:9393/about/waleeta
+  person = params[:person]
+  "#{person} is a programmer, and #{person} is learning Sinatra."
+end 
+
+get "/:person1/loves/:person2" do
+  #query parameter
+  #http://localhost:9393/Matt/loves/Waleeta
+  "#{params[:person1]} loves #{params[:person2]}."
+end 
+
+
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
-# write a basic GET route
-# add a query parameter
-# GET /
-get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
-end
-
-# write a GET route with
-# route parameters
-get '/about/:person' do
-  person = params[:person]
-  "#{person} is a programmer, and #{person} is learning Sinatra."
-end
-
-get '/:person_1/loves/:person_2' do
-  "#{params[:person_1]} loves #{params[:person_2]}"
-end
-
-# write a GET route that retrieves
-# all student data
 get '/students' do
   students = db.execute("SELECT * FROM students")
-  response = ""
-  students.each do |student|
-    response << "ID: #{student['id']}<br>"
-    response << "Name: #{student['name']}<br>"
-    response << "Age: #{student['age']}<br>"
-    response << "Campus: #{student['campus']}<br><br>"
-  end
-  response
+    response = ""
+    students.each do |student|  
+      response << "<b>ID</b>: #{student['id']}<br>"
+      response << "<b>Name</b>: #{student['name']}<br>"
+      response << "<b>Age</b>: #{student['age']}<br>"
+      response << "<b>Campus</b>: #{student['campus']}<br><br>"
+    end
+    response
 end
 
-# write a GET route that retrieves
-# a particular student
 
 get '/students/:id' do
-  student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
-  student.to_s
+  #http://localhost:9393/students/51
+  #query parameter
+  students = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+  students.to_s
 end
+
+# ####################################################################################
+get '/contact' do 
+  "Please contact us at: <br>
+  PO Box 412<br>
+  Chicago, IL <br><br>
+  And give us a call at: <br>
+  1-800-555-1234"
+end 
+
+get '/great_job' do 
+  #http://localhost:9393/great_job?name=waleeta
+  name = params[:name]
+  if name
+    "Good job #{name}!"
+  else 
+    "Great job!"
+  end 
+end 
+
+
+get '/adds/:num1/:num2' do 
+    # num1 = params[:num1.to_s]
+    # num2 = params[:num2.to_s]
+  "#{params[:num1].to_i + params[:num2].to_i}"
+
+end 
