e5883627d752588c9ec02ebaa3c07d5c34e40fe8
Waleeta Canon <waleetacanon@gmail.com>
2016-09-29 09:50:16 -0500

    Adds additional animate effect

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
      $("img").click(function(){
         $("img").animate({height: "+=200px"});
 });
-
+     $("img").click(function(){
+        $("img").animate({height: "-=200px"});
+});
 });
